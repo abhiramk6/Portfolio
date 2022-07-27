86f4193dee606516233970b7556488207b84e8ba
abhiramk6 <84430900+abhiramk6@users.noreply.github.com>
2022-07-27 09:34:47 +0530

    edit

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 from django.contrib.messages import constants as messages
 import os
-import whitenoise
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
